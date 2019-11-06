59a8922af4383468b99bf4d94883cbbe3d42df67
remoo1901 <remoo1901@hotmail.com>
2019-11-05 23:28:25 -0500

    20 passing

--- index.js
+++ index.js
@@ -46,17 +46,15 @@ function Person(name, age) {
   }
   
   Person.prototype.eat = function (someFood){
-  if (this.stomach.length <= 10 ){
+  if (this.stomach.length < 10 ){
     this.stomach.push(someFood)
   }
   };
   
-  Person.prototype.poop = function (){
-    if (this.stomach.pop()){
-      this.stomach.length = 0
-    }
-  } 
-  
+  Person.prototype.poop = function() {
+    this.stomach = []
+   }
+
   Person.prototype.toString = function () {
     return `${this.name} + ${this.age}`
   };
@@ -75,8 +73,15 @@ function Person(name, age) {
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
-  
+function Car(model, milesPerGallon) {
+  this.model = model;
+  this.milesPerGallon = milesPerGallon;
+  this.tank = 0;
+  this.odometer = 0;
+}
+
+Car.prototype.fill = function (gallons) {
+  this.tank = this.tank+gallons;
 }
 
 /*
@@ -86,8 +91,12 @@ function Car() {
     - Besides the methods on Person.prototype, babies have the ability to `.play()`:
         + Should return a string "Playing with x", x being the favorite toy.
 */
-function Baby() {
-
+function Baby(name, age, favoriteToy) {
+  Person.call(this, name, age)
+  this.favoriteToy = favoriteToy;
+}
+Baby.prototype.play = function (){
+  
 }
 
 /* 
