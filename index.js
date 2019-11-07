1552424d544257c7c91e1412ffdfa9e7ac0b17dc
remoo1901 <remoo1901@hotmail.com>
2019-11-06 19:30:39 -0500

    22

--- index.js
+++ index.js
@@ -95,8 +95,10 @@ function Baby(name, age, favoriteToy) {
   Person.call(this, name, age)
   this.favoriteToy = favoriteToy;
 }
+Baby.prototype = Object.create(Person.prototype);
+
 Baby.prototype.play = function (){
-  
+ return "Playing with" +this.favoriteToy 
 }
 
 /* 
